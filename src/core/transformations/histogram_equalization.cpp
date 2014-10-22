a5fcbcc34e60accf83648b3d711ae18c28310713
grzkon <facesyf@gmail.com>
2014-10-22 16:47:46 +0200

    Task 3

--- src/core/transformations/histogram_equalization.cpp
+++ src/core/transformations/histogram_equalization.cpp
@@ -62,65 +62,66 @@ PNM* HistogramEqualization::transform()
 		cha.append(Rcha);
 		cha.append(Gcha);
 		cha.append(Bcha);
-		for (QHash<int, int> cha : cha){
-			QHash<int, int>::const_iterator ite;
-			double dis[256] = { 0 };
-			double bot = width * height;
-			ite = cha.constBegin();
-			while (ite != cha.constEnd())
-			{
-				dis[ite.key()] += ite.value() / bot;
-				ite++;
-			}
-			for (int i = 1; i < 256; i++)
-			{
-				dis[i] += dis[i - 1];
-			}
+		QHash<int, int>::const_iterator Rite;
+		QHash<int, int>::const_iterator Gite;
+		QHash<int, int>::const_iterator Bite;
+		double Rdis[256] = { 0 };
+		double Gdis[256] = { 0 };
+		double Bdis[256] = { 0 };
+		double bot = width * height;
+		Rite = Rcha.constBegin();
+		Gite = Gcha.constBegin();
+		Bite = Bcha.constBegin();
+		while (Rite != Rcha.constEnd())
+		{
+			Rdis[Rite.key()] += Rite.value() / bot;
+			Rite++;
+		}
+		while (Gite != Gcha.constEnd())
+		{
+			Gdis[Gite.key()] += Gite.value() / bot;
+			Gite++;
+		}
+		while (Bite != Bcha.constEnd())
+		{
+			Bdis[Bite.key()] += Bite.value() / bot;
+			Bite++;
+		}
+		for (int i = 1; i < 256; i++)
+		{
+			Rdis[i] += Rdis[i - 1];
+		}
+		for (int i = 1; i < 256; i++)
+		{
+			Gdis[i] += Gdis[i - 1];
+		}
+		for (int i = 1; i < 256; i++)
+		{
+			Bdis[i] += Bdis[i - 1];
+		}
 			for (int x = 0; x < width; x++)
 			{
 				for (int y = 0; y < height; y++)
 				{
 					QRgb pix;
-					QRgb tpix;
 					QColor npix;
 					int red;
 					int gre;
 					int blu;
-					int szu;
-					if (ln == 0)
-					{
-						pix = image->pixel(x, y);
-						red = qRed(pix);
-						gre = qGreen(pix);
-						blu = qBlue(pix);
-						szu = dis[red] * 255;
-						npix = QColor((int)szu, gre, blu);
-					}
-					if (ln == 1)
-					{
-						pix = image->pixel(x, y);
-						tpix = newImage->pixel(x, y);
-						red = qRed(tpix);
-						gre = qGreen(pix);
-						blu = qBlue(pix);
-						szu = dis[gre] *255;
-						npix = QColor(red, (int)szu, blu);
-					}
-					if (ln == 2)
-					{
-						pix = image->pixel(x, y);
-						tpix = newImage->pixel(x, y);
-						red = qRed(tpix);
-						gre = qGreen(tpix);
-						blu = qBlue(pix);
-						szu = dis[blu] * 255;
-						npix = QColor(red, gre, (int)szu);
-					}
+					int Rszu;
+					int Gszu;
+					int Bszu;
+					pix = image->pixel(x, y);
+					red = qRed(pix);
+					gre = qGreen(pix);
+					blu = qBlue(pix);
+					Rszu = Rdis[red] * 255;
+					Gszu = Gdis[gre] * 255;
+					Bszu = Bdis[blu] * 255;
+					npix = QColor((int)Rszu, (int)Gszu, (int)Bszu);
 					newImage->setPixel(x, y, npix.rgb());
 				}
 			}
-			ln++;
-		}
 		
 	}
 
