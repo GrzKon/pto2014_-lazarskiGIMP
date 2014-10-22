3b8e2e538299062a8d7ee7919976e336fab0a135
grzkon <facesyf@gmail.com>
2014-10-22 15:51:09 +0200

    Task 3

--- src/core/transformations/histogram_equalization.cpp
+++ src/core/transformations/histogram_equalization.cpp
@@ -19,7 +19,110 @@ PNM* HistogramEqualization::transform()
 
     PNM* newImage = new PNM(width, height, image->format());
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	if (image->format() == QImage::Format_Indexed8)
+	{
+		Histogram* his = image->getHistogram();
+		his->get(Histogram::LChannel);
+		QHash<int, int> cha = *his->get(Histogram::LChannel);
+		QHash<int, int>::const_iterator ite;
+		double dis[256] = { 0 };
+		double bot = width * height;
+		ite = cha.constBegin();
+		while (ite != cha.constEnd())
+		{
+			dis[ite.key()] += ite.value() / bot;
+			ite++;
+		}
+		for (int i = 1; i < 256; i++)
+		{
+			dis[i] += dis[i - 1];
+		}
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				QRgb pix = image->pixel(x, y);
+				int qgre = qGray(pix);
+				int szu = dis[qgre] * 255;
+				newImage->setPixel(x, y, szu);
+
+			}
+		}
+
+	}
+	else
+	{
+		int ln = 0;
+		Histogram* his = image->getHistogram();
+		his->get(Histogram::RChannel);
+		QList<QHash<int, int>> cha;
+		QHash<int, int> Rcha = *his->get(Histogram::RChannel);
+		QHash<int, int> Gcha = *his->get(Histogram::GChannel);
+		QHash<int, int> Bcha = *his->get(Histogram::BChannel);
+		cha.append(Rcha);
+		cha.append(Gcha);
+		cha.append(Bcha);
+		for (QHash<int, int> cha : cha){
+			QHash<int, int>::const_iterator ite;
+			double dis[256] = { 0 };
+			double bot = width * height;
+			ite = cha.constBegin();
+			while (ite != cha.constEnd())
+			{
+				dis[ite.key()] += ite.value() / bot;
+				ite++;
+			}
+			for (int i = 1; i < 256; i++)
+			{
+				dis[i] += dis[i - 1];
+			}
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					QRgb pix;
+					QRgb tpix;
+					QColor npix;
+					int red;
+					int gre;
+					int blu;
+					int szu;
+					if (ln == 0)
+					{
+						pix = image->pixel(x, y);
+						red = qRed(pix);
+						gre = qGreen(pix);
+						blu = qBlue(pix);
+						szu = dis[red] * 255;
+						npix = QColor((int)szu, gre, blu);
+					}
+					if (ln == 1)
+					{
+						pix = image->pixel(x, y);
+						tpix = newImage->pixel(x, y);
+						red = qRed(tpix);
+						gre = qGreen(pix);
+						blu = qBlue(pix);
+						szu = dis[gre] *255;
+						npix = QColor(red, (int)szu, blu);
+					}
+					if (ln == 2)
+					{
+						pix = image->pixel(x, y);
+						tpix = newImage->pixel(x, y);
+						red = qRed(tpix);
+						gre = qGreen(tpix);
+						blu = qBlue(pix);
+						szu = dis[blu] * 255;
+						npix = QColor(red, gre, (int)szu);
+					}
+					newImage->setPixel(x, y, npix.rgb());
+				}
+			}
+			ln++;
+		}
+		
+	}
 
     return newImage;
 }
--- src/core/transformations/histogram_stretching.cpp
+++ src/core/transformations/histogram_stretching.cpp
@@ -19,9 +19,112 @@ PNM* HistogramStretching::transform()
 
     PNM* newImage = new PNM(width, height, image->format());
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	if (image->format() == QImage::Format_Indexed8)
+	{
+		int min = 255;
+		int max = 0;
+		Histogram* his = image->getHistogram();
+		his->get(Histogram::LChannel);
+		QHash<int, int> cha = *his->get(Histogram::LChannel);
+		QHash<int, int>::const_iterator ite;
+		ite = cha.constBegin();
+		while (ite != cha.constEnd()){
+			if (ite.key() > 0)
+			{
+				if (ite.key() < min) min = ite.key();
+				if (ite.key() > max) max = ite.key();
+			}
+			ite++;
+		}
+		double top = 255.0;
+		double bot = max - min;
+		double str = top / bot;
+		for (int x = 0; x < width; x++){
+			for (int y = 0; y < height; y++){
+				QRgb strpix = image->pixel(x, y);
+				int grapix = qGray(strpix);
+				grapix = str*(grapix - min);
+				newImage->setPixel(x, y, grapix);
+			}
 
-    return newImage;
+		}
+	}
+	else
+	{
+		int ln = 0;
+		Histogram* his = image->getHistogram();
+		his->get(Histogram::RChannel);
+		QList<QHash<int, int>> cha;
+		QHash<int, int> Rcha = *his->get(Histogram::RChannel);
+		QHash<int, int> Gcha = *his->get(Histogram::GChannel);
+		QHash<int, int> Bcha = *his->get(Histogram::BChannel);
+		cha.append(Rcha);
+		cha.append(Gcha);
+		cha.append(Bcha);
+		for (QHash<int, int> cha : cha)
+		{
+			int min = 255;
+			int max = 0;
+			QHash<int, int>::const_iterator ite;
+			ite = cha.constBegin();
+			while (ite != cha.constEnd())
+			{
+				int r = ite.key();
+				if (r > 0)
+				{
+					if (r < min) min = ite.key();
+					if (r > max) max = ite.key();
+				}
+				ite++;
+			}
+			double top = 255.0;
+			double bot = max - min;
+			double str = top / bot;
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					QRgb pix;
+					QRgb tpix;
+					QColor npix;
+					int red;
+					int gre;
+					int blu;
+					double szu;
+					if (ln == 0)
+					{
+						pix = image->pixel(x, y);
+						red = qRed(pix);
+						gre = qGreen(pix);
+						blu = qBlue(pix);
+						szu = str *(red - min);
+						npix = QColor((int)szu, gre, blu);
+					}
+					if (ln == 1)
+					{
+						pix = image->pixel(x, y);
+						tpix = newImage->pixel(x, y);
+						red = qRed(tpix);
+						gre = qGreen(pix);
+						blu = qBlue(pix);
+						szu = str *(gre - min);
+						npix = QColor(red, (int)szu, blu);
+					}
+					if (ln == 2)
+					{
+						pix = image->pixel(x, y);
+						tpix = newImage->pixel(x, y);
+						red = qRed(tpix);
+						gre = qGreen(tpix);
+						blu = qBlue(pix);
+						szu = str *(blu - min);
+						npix = QColor(red, gre, (int)szu);
+					}
+					newImage->setPixel(x, y, npix.rgb());
+				}
+			}
+			ln++;
+		}
+	}
+	return newImage;
 }
-
-
