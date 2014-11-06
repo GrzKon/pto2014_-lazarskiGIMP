0938c91b2e755ba9d2276a43b2a2891bddd964f0
Piotr Ożga <webmajstr@gmail.com>
2014-11-06 15:08:42 +0100

    implemented helper methods for second task

--- src/core/transformations/noise_bilateral.cpp
+++ src/core/transformations/noise_bilateral.cpp
@@ -1,48 +1,62 @@
 #include "noise_bilateral.h"
 
 NoiseBilateral::NoiseBilateral(PNM* img) :
-    Convolution(img)
+Convolution(img)
 {
 }
 
 NoiseBilateral::NoiseBilateral(PNM* img, ImageViewer* iv) :
-    Convolution(img, iv)
+Convolution(img, iv)
 {
 }
 
 PNM* NoiseBilateral::transform()
 {
-    int width  = image->width();
-    int height = image->height();
+	int width = image->width();
+	int height = image->height();
 
-    PNM* newImage = new PNM(width, height, image->format());
+	PNM* newImage = new PNM(width, height, image->format());
 
-    sigma_d = getParameter("sigma_d").toInt();
-    sigma_r = getParameter("sigma_r").toInt();
-    radius = sigma_d;
+	sigma_d = getParameter("sigma_d").toInt();
+	sigma_r = getParameter("sigma_r").toInt();
+	radius = sigma_d;
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	qDebug() << Q_FUNC_INFO << "Not implemented yet!";
 
-    return newImage;
+	return newImage;
 }
 
 int NoiseBilateral::calcVal(int x, int y, Channel channel)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	int width = image->width();
+	int height = image->height();
+	
+	float up = 0.0;
+	float down = 0.0;
+	float part;
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			
+			part = colorCloseness(image->pixel(i, j), image->pixel(x, y)) * spatialCloseness(QPoint(i, j), QPoint(x, y));
 
-    return 0;
+			up += image->pixel(i, j) * part;
+			down += part;
+		}
+	}
+
+	return (int) floor(up / down);
 }
 
 float NoiseBilateral::colorCloseness(int val1, int val2)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	float E = 2.718281828459;
 
-    return 0;
+	return (float)(E - ((pow((double)(val1 - val2), 2.0)) / (2.0 * pow((double)sigma_r, 2.0))));
 }
 
 float NoiseBilateral::spatialCloseness(QPoint point1, QPoint point2)
 {
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	float E = 2.718281828459;
 
-    return 0;
+	return (float)(E - ((pow((double)(point1.x - point2.x), 2.0) + pow((double)(point1.y - point2.y), 2.0)) / (2.0 * pow((double)sigma_d, 2.0))));
 }
