636917a7d99c75bc8aa284a6ee8721325dd60c67
Piotr Ożga <webmajstr@gmail.com>
2014-11-20 14:41:36 +0100

    finished 1 and 2 tasks for edge recognition

--- src/core/transformations/edge_gradient.cpp
+++ src/core/transformations/edge_gradient.cpp
@@ -1,31 +1,68 @@
 #include "edge_gradient.h"
 
 EdgeGradient::EdgeGradient(PNM* img, ImageViewer* iv) :
-    Convolution(img, iv)
+Convolution(img, iv)
 {
 }
 
 EdgeGradient::EdgeGradient(PNM* img) :
-    Convolution(img)
+Convolution(img)
 {
 }
 
 PNM* EdgeGradient::verticalDetection()
 {
-    return convolute(g_y, RepeatEdge);
+	return convolute(g_y, RepeatEdge);
 }
 
 PNM* EdgeGradient::horizontalDetection()
 {
-    return convolute(g_x, RepeatEdge);
+	return convolute(g_x, RepeatEdge);
 }
 
 PNM* EdgeGradient::transform()
 {
-    PNM* newImage = new PNM(image->width(), image->height(), image->format());
+	PNM* newImage = new PNM(image->width(), image->height(), image->format());
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	int width = image->width();
+	int height = image->height();
 
-    return newImage;
+	PNM* imageX = horizontalDetection();
+	PNM* imageY = verticalDetection();
+
+	if (image->format() == QImage::Format_Indexed8)
+	{
+		int q;
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				QRgb xPixel = imageX->pixel(x, y);
+				QRgb yPixel = imageY->pixel(x, y);
+				q = (int)sqrt(pow(qGray(xPixel), 2.0) + pow(qGray(yPixel), 2.0));
+				newImage->setPixel(x, y, q);
+			}
+		}
+	}
+	else
+	{
+		int r, g, b;
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				QRgb xPixel = imageX->pixel(x, y);
+				QRgb yPixel = imageY->pixel(x, y);
+				r = (int)sqrt(pow(qRed(xPixel), 2.0) + pow(qRed(yPixel), 2.0));
+				g = (int)sqrt(pow(qGreen(xPixel), 2.0) + pow(qGreen(yPixel), 2.0));
+				b = (int)sqrt(pow(qBlue(xPixel), 2.0) + pow(qBlue(yPixel), 2.0));
+				newImage->setPixel(x, y, QColor(r, g, b).rgb());
+			}
+		}
+	}
+
+	return newImage;
+
+	return newImage;
 }
 
--- src/core/transformations/edge_laplacian.cpp
+++ src/core/transformations/edge_laplacian.cpp
@@ -1,22 +1,31 @@
 #include "edge_laplacian.h"
 
 EdgeLaplacian::EdgeLaplacian(PNM* img) :
-    Convolution(img)
+Convolution(img)
 {
 }
 
 EdgeLaplacian::EdgeLaplacian(PNM* img, ImageViewer* iv) :
-    Convolution(img, iv)
+Convolution(img, iv)
 {
 }
 
 math::matrix<float> EdgeLaplacian::getMask(int, Mode)
 {
-    int size = getParameter("size").toInt();
-    math::matrix<float> mask(size, size);
+	int size = getParameter("size").toInt();
+	math::matrix<float> mask(size, size);
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+	for (int x = 0; x < size; x++)
+	{
+		for (int y = 0; y < size; y++)
+		{
+			mask(x, y) = -1;
+		}
+	}
 
-    return mask;
+	int halfsize = size / 2;
+	mask(halfsize, halfsize) = ((float)pow(size, 2.0)) - 1.0;
+
+	return mask;
 }
 
